fa34adb8149d412e775694de9bed85fdea1de4ea
agarwalson02 <agarwalson02@gmail.com>
2026-04-16 04:39:10 +0530

    Updated the main

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"go_ecommerce_cart/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+
+	router.Run(":" + port)
+
+}
